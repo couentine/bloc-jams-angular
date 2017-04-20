5be236a0db9f8f5e67cea87be03e7c7f3d3d8973
Quentin Guillaume <quent.guillaume@gmail.com>
2017-04-20 10:02:23 -0700

    checkpoint  7 done

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,9 +1,10 @@
- (function() {
-     function AlbumCtrl(Fixtures) {
-         this.albumData = Fixtures.getAlbum();
-     }
- 
-     angular
-         .module('blocJams')
-         .controller('AlbumCtrl', ['Fixtures', AlbumCtrl]);
- })();
+(function() {
+    function AlbumCtrl(Fixtures, SongPlayer) {
+        this.albumData = Fixtures.getAlbum();
+        this.songPlayer = SongPlayer;
+    }
+    
+    angular
+        .module('blocJams')
+        .controller('AlbumCtrl', ['Fixtures', 'SongPlayer', AlbumCtrl]);
+})();
